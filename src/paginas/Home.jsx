a2c9b399963fa0b7c60ef3f446fed21be44d8abb
Alexander F. Stefani <alexanderferreirastefani@yahoo.com.br>
2026-05-22 01:11:30 -0300

    atualizando a Home

--- src/paginas/Home.jsx
+++ src/paginas/Home.jsx
@@ -1,6 +1,9 @@
 import Header from "../components/Header/Header"
+import Hero from "../components/Hero/Hero"
+import Banner from "../components/Banner/Banner"
 import Footer from "../components/Footer/Footer"
 import Button from "../components/Button/Button"
+import ProjectsList from "../components/ProjectsList/ProjectsList"
 
 function Home(){
     return(
@@ -8,7 +11,14 @@ function Home(){
             <Header/>
             <h1>HOME</h1>
             <p>Subtitulo</p>
-            <Button buttonStyle="pimary" arrow>Olá!</Button>
+            <Button buttonStyle="primary" arrow>Olá!</Button>
+            <div className="container">
+                <Hero/>
+                <ProjectsList/>
+            </div>
+           
+            <Banner title="Home" image="about.jpg"/>
+            
             <Footer/>
         </>
     )
