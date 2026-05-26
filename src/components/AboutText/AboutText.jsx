f763912e1a10fea60408d3fec934bd4a68de93a4
Alexander F. Stefani <alexanderferreirastefani@yahoo.com.br>
2026-05-26 20:35:07 -0300

    Criando AboutText

--- src/components/AboutText/AboutText.jsx
+++ src/components/AboutText/AboutText.jsx
@@ -1,1 +1,18 @@
+import './AboutText.css'
 
+function AboutText(props){
+    return(
+          <div className='container'>
+            <div className='text-section d-flex'>
+                <h2>ABC SER</h2>
+            </div>
+            <div className='text-section d-flex'>
+                <p className="primary-color">Primeira linha</p>
+                <p className="primary-color">Segunda linha</p>
+                <p className="primary-color">Terceira linha</p>
+            </div>
+          </div>
+    )
+}
+
+export default AboutText
