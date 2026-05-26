2ef21607a410ba58003ae60a5e3867952417e53f
Alexander F. Stefani <alexanderferreirastefani@yahoo.com.br>
2026-05-26 20:03:32 -0300

    atualizando pagina Projects

--- src/paginas/Projects.jsx
+++ src/paginas/Projects.jsx
@@ -1,8 +1,18 @@
+import Header from "../components/Banner/Banner"
+import Hero from "../components/Hero/Hero"
+import Banner from "../components/Banner/Banner"
+import Footer from "../components/Footer/Footer"
+import ProjectsList from "../components/ProjectsList/ProjectsList"
+
 function Projects(){
     return(
         <>
-            <h1>PROJECTS</h1>
-            <h2>Subtitulo projects</h2>
+            <Header />
+            <Banner title ="Projects" image="project.jpg" />
+            <div className="container">
+                <ProjectList />
+            </div>
+            <Footer />
         </>
     )
 }
