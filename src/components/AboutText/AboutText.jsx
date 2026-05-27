fd6c96d68dc1bd982cf25c1e323c94901e57165b
Alexander F. Stefani <alexanderferreirastefani@yahoo.com.br>
2026-05-27 04:17:20 -0300

    Refazendo estrutura AboutText

--- src/components/AboutText/AboutText.jsx
+++ src/components/AboutText/AboutText.jsx
@@ -2,16 +2,18 @@ import './AboutText.css'
 
 function AboutText(props){
     return(
-          <div className='container'>
-            <div className='text-section d-flex'>
-                <h2>ABC SER</h2>
-            </div>
-            <div className='text-section d-flex'>
-                <p className="primary-color">Primeira linha</p>
-                <p className="primary-color">Segunda linha</p>
-                <p className="primary-color">Terceira linha</p>
-            </div>
-          </div>
+        <div className="container">
+             <div className='text-section d-flex'>
+                <div className='text-section-text'>
+                    <h2>ABC SER</h2>
+                </div>
+                <div className='text-section-text'>
+                    <p className="primary-color">Primeira linha</p>
+                    <p className="primary-color">Segunda linha</p>
+                    <p className="primary-color">Terceira linha</p>
+                </div>
+              </div>
+        </div>  
     )
 }
 
