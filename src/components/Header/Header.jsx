c4bbcc0feac6e923e4785a839a896183fa3b6f67
Alexander F. Stefani <alexanderferreirastefani@yahoo.com.br>
2026-06-04 04:11:24 -0300

    Reformulando a função do botão menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,18 +10,21 @@ import Button from '../Button/Button'
 
 function Header(){
     const [isOpen, setIsOpen] = useState(false)
+    const toggleMenu = () => {
+        setIsOpen(!isOpen  )
+    }
     return(
         <header>
             <div className="container">
                 <div className="al-center d-flex jc-space-between">
                     <Link to="/"><img src={Logo} alt="" /></Link>  {/* Esta linha contém a logo e a transforma em um link que joga para a página principal*/}
                     <div className="mobile-menu">
-                        <Button buttonStyle="secondary" onClick={() => setIsOpen(!isOpen)}>
+                        <Button buttonStyle="secondary" onClick={toggleMenu}>
                             Menu
                         </Button>
                     </div>
                     <nav className={`${isOpen ? 'open' : ''}`}>
-                        <Button buttonStyle="unstyled" className="mobile-menu close-btn" onClick={() => setIsOpen(!isOpen)}>
+                        <Button buttonStyle="unstyled" className="mobile-menu close-btn" onClick={toggleMenu}>
                             X
                         </Button>
                         <ul className='d-flex'>
