a0ac2e7963724de9864069d1fb8e022475103034
Alexander F. Stefani <alexanderferreirastefani@yahoo.com.br>
2026-06-04 03:28:12 -0300

    Add Button component to Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,12 +4,20 @@ import './Header.css'
 //ASSETS
 import Logo from '../../assets/dnc-logo.svg'
 
+//COMPONENTS
+import Button from '../Button/Button'
+
 function Header(){
     return(
         <header>
             <div className="container">
                 <div className="al-center d-flex jc-space-between">
                     <Link to="/"><img src={Logo} alt="" /></Link>  {/* Esta linha contém a logo e a transforma em um link que joga para a página principal*/}
+                    <div>
+                        <Button buttonStyle="secondary">
+                            Menu
+                        </Button>
+                    </div>
                     <nav>
                         <ul className='d-flex'>
                             <li><Link to="/">Home</Link></li>
