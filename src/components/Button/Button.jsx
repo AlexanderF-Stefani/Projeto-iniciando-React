18f3a094e14a6dcc2c040c01d40272eecd0a76ac
AlexanderF-Stefani <alexanderferreirastefani@yahoo.com.br>
2026-07-29 06:59:14 -0300

    CorrecaoDeDigitaçaoEmDiversosArquivos

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,11 +1,10 @@
-import './Header.css'
+import './Button.css'
 import WhiteArrow from '../../assets/white-arrow.svg'
 
-//ASSETS
 
 function Button({arrow, buttonStyle, loading, children, ...props}){
     return(
-        <button className={`button${buttonStyle}`} {...props}>
+        <button className={`button ${buttonStyle}`} {...props}>
             {children} {arrow  && <img src={WhiteArrow}/>}
         </button>
     )
